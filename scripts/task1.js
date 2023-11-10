f263191c684ed986507f9cbcca59a4a94762a8f2
AMD_RYZEN_5_7600X <arca26100@gmail.com>
2023-11-10 18:11:46 +0600

    initial commit

--- scripts/task1.js
+++ scripts/task1.js
@@ -1,60 +1,69 @@
-function longestString(string){
-    const arr = string.split(' ')
-    let arr2 = []
-    arr.forEach((element) => {
-        arr2.push(element.length)
+function task1(input){
+    function counting(arr, elem){
+        let count = 0;
+        for(var i = 0; i < arr.length; ++i){
+            if(arr[i] == elem)
+                count++;
+        }
+        return count
+    }
+    for(let i = 0; i < input.length; i++){
+        if(counting(input, input[i]) == 1){
+            return input[i];
+        }
+    }
+}
+
+function task2(input){
+    const res = []
+    for(let i = 1; i < input.length; i++){
+        res.push(input.slice(0, i))
+    }
+    for(let i = 0; i < input.length; i++){
+        res.push(input.slice(i, input.length))
+    }
+    return res
+}
+
+function task3(input){
+    let check = 'euioa'
+    let counter = 0
+    for(let i = 0; i < input.length; i++){
+        let element = input[i]
+        if (check.includes(element)) {
+            counter++
+        }
+    }
+    return counter
+}
+
+function task4(){
+    const now = new Date()
+    const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const day = week[now.getDay()]
+    let partOfDay = 'AM'
+    let Hour = now.getHours()
+    if (Hour > 12){
+        partOfDay = 'PM'
+        Hour -= 12
+    }
+    const Minute = now.getMinutes()
+    const Second = now.getSeconds()
+    return `today is : ${day}.\nCurrent time is : ${Hour} ${partOfDay} : ${Minute} : ${Second}`
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    const element = document.getElementById("input");
+    const btn = document.getElementById("play");
+
+    element.addEventListener("keydown", function(e) {
+        if(e.key == "Enter"){
+            alert('enter is pressed')
+        }
+    });
+    btn.addEventListener("click", function() {
+        document.getElementById("audio").play()
     })
-    let max = arr2[0]
-    let maxind = 0;
-    for (let i = 0; i < arr2.length; i++) {
-        if (max < arr2[i]){
-            max = arr2[i]
-            maxind = i
-        }
-    }
-    console.log(arr[maxind])
-}
+});
 
-function slsb(arr){
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length; j++){
-            if(arr[j] > arr[j+1]){
-                let temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
-            }
-        }
-    }
-    console.log(`${arr[1]}, ${arr[arr.length-2]}`)
-}
-
-function mySumm(arr){
-    let res = 0
-    for(let i = 0; i < arr.length; i++){
-        res += arr[i]
-    }
-    console.log(res)
-}
-
-function oddEven(num){
-    if(num % 2 == 0){
-        console.log(`${num} is even`)
-    }
-    else{
-        console.log(`${num} is odd`)
-    }
-}
-
-function max(arr){
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length; j++){
-            if(arr[j] < arr[j+1]){
-                let temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
-            }
-        }
-    }
-    console.log(arr[0])
-}
 
